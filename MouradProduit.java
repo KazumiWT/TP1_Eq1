d1937eb875ae1daf4043d2e85def2c44c37415d0
Mourad0906 <m.mouradchikhi@gmail.com>
2024-10-30 16:39:52 -0400

    Creation de la classe Produit

---
+++ MouradProduit.java
@@ -0,0 +1,11 @@
+public class MouradProduit {
+    private String code;
+    private String designation;
+    private double prix;
+
+    public MouradProduit(String code, String designation, double prix) {
+        this.code = code;
+        this.designation = designation;
+        this.prix = prix;
+    }
+}
